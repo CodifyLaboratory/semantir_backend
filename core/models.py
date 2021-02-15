6b19bb8ab7b2791908cce0afac3e52230a0e44ee
sultan <sultan.panar@gmail.com>
2021-02-15 18:46:18 +0600

    added new models

--- core/models.py
+++ core/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
-# Create your models 
+ 
 class Products(models.Model):
     user = models.ForeignKey(
         to=User,
@@ -13,7 +12,7 @@ class Products(models.Model):
     name = models.CharField(max_length=255, verbose_name="Название продукта")
     text = models.TextField(max_length=400, verbose_name="Текст")
     image = models.ImageField(verbose_name="Фотография продукта")
-    
+
 class Blogs(models.Model):
     title = models.CharField(max_length=255, verbose_name="Заголовок статьи")
     text = models.TextField(verbose_name="Текст статьи")
@@ -27,18 +26,17 @@ class Partners(models.Model):
     partners_image = models.ImageField(verbose_name="Фотография партнера")
 
 class User(models.Model):
-    user=models.OneToOneField(User,on_delete=models.CASCADE,related_name="profile")
-    first_name=models.TextField(blank=False,null=True)
-    emil = models.EmailField(blank=False, max_length=254, )
+    user= models.ForeignKey("KeyText",on_delete=models.CASCADE, related_name="profile")
+    email = models.EmailField(blank=False, max_length=254, )
     date_joined=models.DateTimeField(auto_now_add=True)
     is_organizer=models.BooleanField(default=False)
     
 class KeyText(models.Model):
+    gen_text=models.OneToOneField("GenText", on_delete=models.CASCADE, null= True, blank=True, related_name="keytext")
     key_word = models.TextField(max_length=500, verbose_name="Текст для генерации")
     created_keytext_date = models.DateTimeField(auto_now_add=True, )
     
-
-
 class GenText(models.Model):
     gen_text = "key_word" + "some gentext"
     created_gentext_date = models.DateTimeField(auto_now_add=True, )
+
--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets, permissions
 from rest_framework.generics import ListAPIView, CreateAPIView,\
-    UpdateAPIView, DestroyAPIView
+    UpdateAPIView, DestroyAPIView, RetrieveAPIView
 from .serializers import *
 from .models import *
 
@@ -9,84 +9,33 @@ class ProductsList(ListAPIView):
     serializer_class = ProductsSerializer
     permission_classes = [permissions.AllowAny]
 
-class ProductsCreate(CreateAPIView):
+class ProductsRetrieve(RetrieveAPIView):
     queryset = Products.objects.all()
     serializer_class = ProductsSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def post(self, request, *args, **kwargs):
-        print("Product created!")
-        return super().post(request, *args, **kwargs)
-
-class ProductsDetail(ListAPIView):
-    queryset = Products.objects.all()
-    serializer_class = ProductsSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class ProductsUpdate(UpdateAPIView):
-    queryset = Products.objects.all()
-    serializer_class = ProductsSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class ProductsDestroy(DestroyAPIView):
-    queryset = Products.objects.all()
-    serializer_class = ProductsSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
 
 class BlogsList(ListAPIView):
     queryset = Blogs.objects.all()
     serializer_class = BlogsSerializer
     permission_classes = [permissions.AllowAny]
 
-class BlogsCreate(CreateAPIView):
+class BlogsRetrieve(RetrieveAPIView):
     queryset = Blogs.objects.all()
     serializer_class = BlogsSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class BlogsDetail(ListAPIView):
-    queryset = Blogs.objects.all()
-    serializer_class = BlogsSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class BlogsUpdate(UpdateAPIView):
-    queryset = Blogs.objects.all()
-    serializer_class = BlogsSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class BlogsDestroy(DestroyAPIView):
-    queryset = Blogs.objects.all()
-    serializer_class = BlogsSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-
+    permission_classes = [permissions.AllowAny]
 
 class PartnersList(ListAPIView):
     queryset = Partners.objects.all()
     serializer_class = PartnersSerializer
     permission_classes = [permissions.AllowAny]
 
-class PartnersCreate(CreateAPIView):
+class PartnersRetrieve(RetrieveAPIView):
     queryset = Partners.objects.all()
     serializer_class = PartnersSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class PartnersDetail(ListAPIView):
-    queryset = Partners.objects.all()
-    serializer_class = PartnersSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class PartnersUpdate(UpdateAPIView):
-    queryset = Partners.objects.all()
-    serializer_class = PartnersSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class PartnersDestroy(DestroyAPIView):
-    queryset = Partners.objects.all()
-    serializer_class = PartnersSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
 
 class KeyTextCreate(CreateAPIView):
     queryset = KeyText.objects.all()
     serializer_class = KeyTextSerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.IsAuthenticated]
 
