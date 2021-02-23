6368df12bfdc12c0f9dc8af3c715cfb36eb90918
AizadaK <kasenova.aizada@gmail.com>
2021-02-23 22:55:35 +0600

    corrected views, urls and admin

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import KeyText
+from .models import KeyText, GenText
 
 admin.site.register(KeyText)
+admin.site.register(GenText)
 # Register your models here.
