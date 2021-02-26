a247f7527c41871a2e892f34271ee9391d881061
AizadaK <kasenova.aizada@gmail.com>
2021-02-26 15:10:35 +0600

    added djoser authtoken in urls and settings, added user in admin, changed requirements

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import KeyText, GenText
+from .models import KeyText, GenText, User
 
 admin.site.register(KeyText)
 admin.site.register(GenText)
+admin.site.register(User)
 # Register your models here.
