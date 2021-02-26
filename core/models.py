ba646cbf836dc57e0023797168b117b7fd65f121
sultan <sultan.panar@gmail.com>
2021-02-26 15:10:06 +0600

    added model User

--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class KeyText(models.Model):
     word = models.CharField(max_length=250, verbose_name="Ключевое слово")
@@ -20,3 +21,8 @@ class GenText(models.Model):
     gen_text = models.TextField(verbose_name="Сгенерированный текст")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата добавления")        
     
+class User(models.Model):
+    user = models.ForeignKey(KeyText, on_delete=models.CASCADE, related_name="profile")
+    email = models.EmailField(blank=False, max_length=254, )
+    date_joined=models.DateTimeField(auto_now_add=True)
+    is_organizer=models.BooleanField(default=False)
