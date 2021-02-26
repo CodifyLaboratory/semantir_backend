e0d4b147580c0a51c8cea2d9145af4032deb4247
AizadaK <kasenova.aizada@gmail.com>
2021-02-26 16:14:26 +0600

    added products, blogs and partners in serializers

--- core/serializers.py
+++ core/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import KeyText, GenText
+from .models import KeyText, GenText, Products, Blogs, Partners
 
 class KeyTextSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,4 +9,20 @@ class KeyTextSerializer(serializers.ModelSerializer):
 class GenTextSerializer(serializers.ModelSerializer):
     class Meta:
         model = GenText
-        fields = ["gen_text"]        
+        fields = ["gen_text"]
+
+class ProductsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Products
+        fields = ["id","name", "text", "image"]
+
+class BlogsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Blogs
+        fields = ["id","title", "text", "publicated_date", "blogs_image"]
+
+
+class PartnersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Partners
+        fields = ["id","partners_image"]                
