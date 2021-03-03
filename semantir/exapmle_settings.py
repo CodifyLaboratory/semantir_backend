02fc9157b6d9acc4dd054b50915d5efbb939be59
Sultan <72707087+panarbekov@users.noreply.github.com>
2021-03-03 19:22:18 +0600

    Update exapmle_settings.py

--- semantir/exapmle_settings.py
+++ semantir/exapmle_settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jvem6pa29@nmisn)!i$!l8(u6zqp1(a+6@=jb-7dxenex0tosq'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
